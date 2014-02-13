81ce6b4979ecc58842f9fc46983fced4085bea32
tungntse60609@fpt.edu.vn <tungntse60609@fpt.edu.vn@94bb71cc-a564-f776-b82a-dbf9a96afb58>
2014-02-13 02:30:06 +0000

    modify database,add function AddRegister - tungnt

--- trunk/Captone/Captone/Controllers/HomeController.cs
+++ trunk/Captone/Captone/Controllers/HomeController.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using System.Data.Entity;
+using Captone.Models;
+using System.IO;
 namespace Captone.Controllers
 {
     public class HomeController : Controller
     {
         //
         // GET: /Home/
-
+        private iDeliverEntities _db = new iDeliverEntities();
         public ActionResult Index()
         {
             return View();
@@ -27,6 +29,24 @@ namespace Captone.Controllers
         {
             return View();
         }
+        public ActionResult SuccessRegister()
+        {
+            return View();
+        }
 
+        public ActionResult AddRegister(FormCollection col)
+        {
+            Account account = new Account();
+            account.Email = col["Email"];
+            account.Username = col["Username"];
+            account.Password = col["Password"];
+            account.Phone = col["Phone"];
+            account.IsAdmin = false;
+            account.RoleID = 3;
+            _db.Accounts.Add(account);
+            _db.SaveChanges();
+            return RedirectToAction("SuccessRegister");
+        }
+        
     }
 }
