ed2b421188832129b7bd8c4b386c9344fb6087b5
longdbse60690@fpt.edu.vn <longdbse60690@fpt.edu.vn@94bb71cc-a564-f776-b82a-dbf9a96afb58>
2014-02-27 13:31:07 +0000

    Update IAssigningService.cs #LongDB

---
+++ trunk/Captone/Captone/Services/IAssigningService.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Captone.Services
+{
+    interface IAssigningService
+    {
+        bool checkForward(string coordinateA, string coordinateB, string coordinateC);
+        System.Collections.Generic.List<Captone.Models.Route> ProcessingRequest(Captone.Models.Request request);
+    }
+}
