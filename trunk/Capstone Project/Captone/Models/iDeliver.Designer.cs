661792a7d9fe953ac09b09d0175759a05519eb5b
longdbse60690@fpt.edu.vn <longdbse60690@fpt.edu.vn@94bb71cc-a564-f776-b82a-dbf9a96afb58>
2014-04-01 13:03:42 +0000

    Commit: - script mới iDeliver 01-04 - xóa project cũ - upload project mới (giao diện tạm ổn, chức năng bên Staff còn thiếu Route, Stage; phần Rating chưa kéo rate sao được) #LongDB

---
+++ trunk/Capstone Project/Captone/Models/iDeliver.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'C:\Users\winvn_000\Desktop\Backup\CP.v1\Captone\Models\iDeliver.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
