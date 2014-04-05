40a4f158b5778ad4bd4f3b0534ac9083b318300f
longdbse60690@fpt.edu.vn <longdbse60690@fpt.edu.vn@94bb71cc-a564-f776-b82a-dbf9a96afb58>
2014-04-05 20:35:00 +0000

    TopStation, UI #LongDB

---
+++ branches/Capstone Project/Capstone.Test/AssigningServiceTest1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Captone.Models;
+using Captone.Repositories;
+using Captone.Repositories.Interfaces;
+using Captone.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Capstone.Test
+{
+    [TestClass]
+    public partial class AssigningServiceTest1
+    {
+        //setup mock object
+        private readonly Mock<GenericRepository<Route>> _route = new Mock<GenericRepository<Route>>();
+        private readonly Mock<GenericRepository<Station>> _station = new Mock<GenericRepository<Station>>();
+        private readonly Mock<GenericRepository<Invoice>> _invoice = new Mock<GenericRepository<Invoice>>();
+        private readonly Mock<GenericRepository<Trip>> _trip = new Mock<GenericRepository<Trip>>();
+        private readonly Mock<GenericRepository<Request>> _request = new Mock<GenericRepository<Request>>();
+        private readonly Mock<GenericRepository<Assigning>> _assigning = new Mock<GenericRepository<Assigning>>();
+        private readonly Mock<GenericRepository<Stage>> _stage = new Mock<GenericRepository<Stage>>();
+        private readonly Mock<GenericRepository<RouteStage>> _routeStage = new Mock<GenericRepository<RouteStage>>();
+
+        DateTime now = new DateTime(2014,04,02,15,30, 0);
+        TimeSpan datePost = new TimeSpan(05,0,0);
+
+        [TestMethod]
+        //[ExpectedException(typeof(NullReferenceException))]
+        public void MainFlow_Test()
+        {
+            // Description of this test case:
+            // There are routes and trips available between:
+            // SG - Đà Lạt, SG - Vũng Tàu, Vũng Tàu - Đà Nẵng, SG - Đà Nẵng
+            // Request1: SG - Vũng Tàu, Request2: SG - Đà Nẵng
+            // Expected result is one request dedicated to one trip
+            //setup input / testcase
+            List<Request> requests = new List<Request>
+                {
+                    new Request{RequestID = 1,
+                                FromLocation = 1,
+                                ToLocation = 2,
+                                DateRequest = now,
+                                DeliveryStatusID = 2},
+                    new Request{RequestID = 2,
+                                FromLocation = 1,
+                                ToLocation = 3,
+                                DateRequest = now,
+                                DeliveryStatusID = 2},
+                };
+            var trips = new List<Trip>
+                {
+                    new Trip{TripID = 1, 
+                             EstimateArrivalTime = datePost,
+                             EstimateDepartureTime = datePost,
+                             AvailableVolume = 3.0,
+                             Date = now,
+                             RouteID = 1,
+                             IsActive = true,
+                    },
+                    new Trip{TripID = 2, 
+                             EstimateArrivalTime = datePost,
+                             EstimateDepartureTime = datePost,
+                             AvailableVolume = 5.0,
+                             Date = now,
+                             RouteID = 1,
+                             IsActive = true,
+                    },
+                    new Trip{TripID = 4, 
+                             EstimateArrivalTime = datePost,
+                             EstimateDepartureTime = datePost,
+                             AvailableVolume = 1.5,
+                             Date = now,
+                             RouteID = 2,
+                             IsActive = true,
+                    },
+                    new Trip{TripID = 3, 
+                             EstimateArrivalTime = datePost.Add(new TimeSpan(5,0,0)),
+                             EstimateDepartureTime = datePost,
+                             AvailableVolume = 1.5,
+                             Date = now,
+                             RouteID = 2,
+                             IsActive = true,
+                    },
+                };
+
+            //data set into mock object
+            var routes = new List<Route>
+                {
+                    new Route{RouteID = 1,
+                              RouteName = "Sài Gòn - Vũng Tàu",
+                              },
+                    new Route{RouteID = 2,
+                              RouteName = "Sài Gòn - Đà Lạt",
+                              },
+                };
+            var stages = new List<Stage>
+                {
+                    new Stage{StageID = 1,
+                                StartPoint = 1,
+                                EndPoint = 2,
+                                },
+                    new Stage{StageID = 2,
+                                StartPoint = 1,
+                                EndPoint = 3,
+                                },
+                    //new Stage{StageID = 1,
+                    //            FromLocation = 1,
+                    //            ToLocation = 1,
+                    //            },
+                };
+            var routeStage = new List<RouteStage>
+                {
+                    new RouteStage{StageID = 1,
+                                RouteID = 1,
+                                },
+                    new RouteStage{StageID = 2,
+                                RouteID = 2,
+                                },
+                };
+            var stations = new List<Station>
+                {
+                    new Station{StationID = 1,
+                                StationName = "SG.Lê Hồng Phong",
+                                StationLocation = "233 Lê Hồng Phong, F4, Q.5, TP Hồ Chí Minh",
+                                Coordinate = "10.7584848, 106.6777557",
+                                },
+                    new Station{StationID = 2,
+                                StationName = "Tạ Uyên - VT",
+                                StationLocation = "03 Tạ Uyên, Vũng Tàu",
+                                Coordinate = "10.3491804, 107.0855695",
+                                },
+                    new Station{StationID = 3,
+                                StationName = "Bến xe liên tỉnh Đà Lạt",
+                                StationLocation = "01 Tô Hiến Thành, F3, Đà Lạt",
+                                Coordinate = "11.9264949, 108.4460601",
+                                },
+                    new Station{StationID = 4,
+                                StationName = "Bến Xe Trung Tâm Đà Nẵng",
+                                StationLocation = "201 Tôn Đức Thắng, Phường Hòa Minh, Liên Chiểu, TP.Đà Nẵng",
+                                Coordinate = "16.051571, 108.214897",
+                                },
+                };
+            var invoices = new List<Invoice>
+                {
+                    new Invoice{InvoiceID = 1,
+                                RequestID = 1,
+                                Volume = 4,
+                                Weight = 2,
+                                Price = 5.6},
+                    new Invoice{InvoiceID = 2,
+                                RequestID = 2,
+                                Volume = 1.5,
+                                Weight = 2,
+                                Price = 5.6},
+                };
+
+            //setup system under test
+            var sut = new AssigningService(_route.Object, _station.Object, _invoice.Object, _trip.Object,
+                                           _request.Object, _assigning.Object, _stage.Object, _routeStage.Object);
+
+            _route.Setup(f => f.GetAll()).Returns(new EnumerableQuery<Route>(routes));
+            _station.Setup(f => f.GetAll()).Returns(new EnumerableQuery<Station>(stations));
+            _invoice.Setup(f => f.GetAll()).Returns(new EnumerableQuery<Invoice>(invoices));
+            _stage.Setup(f => f.GetAll()).Returns(new EnumerableQuery<Stage>(stages));
+            _trip.Setup(f => f.GetAll()).Returns(new EnumerableQuery<Trip>(trips));
+            _routeStage.Setup(f => f.GetAll()).Returns(new EnumerableQuery<RouteStage>(routeStage));
+
+            //test it
+            var result = sut.Assigning(requests);
+
+            Assert.AreEqual(requests.Count, result.Count);
+        }
+    }
+}
---
+++ branches/Capstone Project/Captone/Controllers/TripController.cs
@@ -0,0 +1,464 @@
+﻿using Captone.Models;
+using Captone.Services;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Services;
+
+namespace Captone.Controllers
+{
+    public class TripController : Controller
+    {
+        private iDeliverEntities db = new iDeliverEntities();
+
+        #region CRUD
+        public ActionResult Index()
+        {
+            //if (Session["USERNAME"] == null)
+            //{
+            //    return RedirectToAction("LogIn", "Account");
+            //}
+            //else
+            //{
+            //    var trips = db.Trips;
+            //    return View(trips.ToList());
+            //}
+            var trips = db.Trips;
+            return View(trips.ToList());
+        }
+
+        //
+        // POST: /Trip/ListTrip
+        [HttpPost]
+        public ActionResult ListTrip(DateTime tripDate)
+        {
+            var trips = db.Trips.Where(t => t.Date == tripDate);
+            return View(trips.ToList());
+        }
+
+        //
+        // GET: /Trip/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            if (Session["USERNAME"] == null)
+            {
+                return RedirectToAction("LogIn", "Account");
+            }
+            else
+            {
+                Trip trip = db.Trips.Find(id);
+                if (trip == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(trip);
+            }
+        }
+
+        //
+        // GET: /Trip/Create
+
+        [WebMethod]
+        public ActionResult Create()
+        {
+            string username = (string)Session["USERNAME"];
+            string role = (string)Session["UserRole"];
+            if (username != null && role == "Staff")
+            {
+                ViewBag.RouteID = new SelectList(db.Routes, "RouteID", "RouteName");
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("LogIn", "Account");
+            }
+        }
+
+        //
+        // POST: /Trip/Create
+
+        [HttpPost]
+        [WebMethod]
+        public ActionResult Create(List<Trip> trips)
+        {
+            if (ModelState.IsValid)
+            {
+                if (trips != null)
+                {
+                    foreach (var trip in trips)
+                    {
+                        Trip t = new Trip();
+                        t.EstimateDepartureTime = trip.EstimateDepartureTime;
+                        t.EstimateArrivalTime = trip.EstimateArrivalTime;
+                        t.RouteID = trip.RouteID;
+                        t.CoachID = trip.CoachID;
+                        t.ScheduleID = trip.ScheduleID;
+                        // Estimate volume = Capacity of coach run this trip * max volume level of route where trip run
+                        t.EstimateVolume = trip.EstimateVolume;
+                        t.AvailableVolume = trip.EstimateVolume;
+                        t.IsActive = true;
+                        t.Status = "Chưa chạy";
+                        for (int i = 0; i < 7; i++)
+                        {
+                            TimeSpan day = new TimeSpan();
+                            if (i == 0)
+                            {
+                                day = new TimeSpan(0, 0, 0, 0);
+                            }
+                            else
+                            {
+                                day = new TimeSpan(i / i, 0, 0, 0);
+                            }
+                            DateTime nextday = trip.Date.Add(day);
+                            trip.Date = nextday.Date;
+                            t.Date = trip.Date.Date;
+                            db.Trips.Add(t);
+                            db.SaveChanges();
+                        }
+                    }
+                    return RedirectToAction("Index");
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        //
+        // GET: /Trip/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            if (Session["USERNAME"] == null)
+            {
+                return RedirectToAction("LogIn", "Account");
+            }
+            else
+            {
+                Trip trip = db.Trips.Find(id);
+                ViewBag.RouteID = new SelectList(db.Routes, "RouteID", "RouteName");
+                ViewBag.CoachID = new SelectList(db.Coaches, "CoachID", "NumberPlate");
+                if (trip == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(trip);
+            }
+        }
+
+        //
+        // POST: /Trip/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Trip trip)
+        {
+            if (Session["USERNAME"] == null)
+            {
+                return RedirectToAction("LogIn", "Account");
+            }
+            else
+            {
+                ViewBag.RouteID = new SelectList(db.Routes, "RouteID", "RouteName");
+                ViewBag.CoachID = new SelectList(db.Coaches, "CoachID", "NumberPlate");
+                if (ModelState.IsValid)
+                {
+                    db.Entry(trip).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(trip);
+            }
+        }
+
+        //
+        // GET: /Trip/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            if (Session["USERNAME"] == null)
+            {
+                return RedirectToAction("LogIn", "Account");
+            }
+            else
+            {
+                Trip trip = db.Trips.Find(id);
+                if (trip == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(trip);
+            }
+        }
+
+        //
+        // POST: /Trip/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Trip trip = db.Trips.Find(id);
+            db.Trips.Remove(trip);
+            Assigning asg = db.Assignings.Where(a => a.TripID == id).FirstOrDefault();
+            db.Assignings.Remove(asg);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        #endregion
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+
+        public ActionResult ListTripForRequest()
+        {
+            var listTrip = db.Trips.ToList();
+            return PartialView("ListTripForRequest", listTrip);
+        }
+
+        [HttpPost]
+        [WebMethod]
+        public void Assigning(List<Request> request)
+        {
+            AssigningService assign = new AssigningService();
+
+            var result = assign.Assigning(request);
+            foreach (var item in result)
+            {
+                for (var i = 0; i < item.Value.Count; i++)
+                {
+                    Assigning ass = new Assigning();
+                    var requestId = item.Key.RequestID;
+                    ass.RequestID = requestId;
+                    var tripId = item.Value.Keys.ToList();
+                    var stop = item.Value.Values.ToList();
+                    ass.TripID = tripId[i].TripID;
+                    ass.StopStation = stop[i];
+                    ass.IndicateOrder = i + 1;
+                    ass.AssignedDate = DateTime.Now;
+                    var req = db.Requests.Where(p => p.RequestID == requestId).Single();
+                    req.DeliveryStatusID = 3;
+                    db.Assignings.Add(ass);
+                    db.SaveChanges();
+                }
+            }
+        }
+
+        //Pass estimated time to real time of trip when depart/arrive
+        [HttpPost]
+        [WebMethod]
+        public ActionResult TimePassing(List<Trip> trips)
+        {
+            if (trips != null)
+            {
+                foreach (var trip in trips)
+                {
+                    DateTime current = DateTime.Now;
+                    if (trip.Date.ToShortDateString() == current.ToShortDateString())
+                    {
+                        Trip t = db.Trips.Where(tr => tr.TripID == trip.TripID).FirstOrDefault();
+                        t.RealDepartureTime = trip.EstimateDepartureTime;
+                        t.RealArrivalTime = trip.EstimateArrivalTime;
+                        db.Entry(t).State = EntityState.Modified;
+                        db.SaveChanges();
+                        return RedirectToAction("Index", "Request");
+                    }
+                }
+            }
+            return View();
+        }
+
+        //For view assigned request of selected trip
+        [HttpPost]
+        [WebMethod]
+        public ActionResult AssignedRequest(int tripID)
+        {
+            List<Request> listRequests = new List<Request>();
+            List<int> rqID = new List<int>();
+            rqID = (from a in db.Assignings where a.TripID == tripID select a.RequestID).ToList();
+            foreach (var id in rqID)
+            {
+                Request rq = db.Requests.Where(r => r.RequestID == id).FirstOrDefault();
+                listRequests.Add(rq);
+            }
+            return View(listRequests);
+        }
+
+        #region load and re-assigned requests of selected trip to be deleted
+        // Retrieve requests assigned to selected trip, find by TripID
+        [HttpPost]
+        [WebMethod]
+        public ActionResult RelatedRequests(int tripID)
+        {
+            List<Request> listRequests = new List<Request>();
+            List<int> rqID = new List<int>();
+            rqID = (from a in db.Assignings where a.TripID == tripID select a.RequestID).ToList();
+            foreach (var id in rqID)
+            {
+                Request rq = db.Requests.Where(r => r.RequestID == id).FirstOrDefault();
+                listRequests.Add(rq);
+            }
+            return View(listRequests);
+        }
+        // Set status of related request to 'Đã xác nhận - 2' to re-assigned
+        [HttpPost]
+        [WebMethod]
+        public void UpdateRequests(int tripID)
+        {
+            List<Trip> _trips = new List<Trip>();
+            List<Assigning> _assignings = db.Assignings.ToList();
+            List<Request> listRequest = new List<Models.Request>();
+
+            // list of requestIDs to be updated/re-assigned
+            var tmpRequestID = new List<int>();
+
+            foreach (var assigning in _assignings)
+            {
+                if (assigning.TripID == tripID)
+                {
+                    // get list of request id that related to delete trip
+                    tmpRequestID.Add(assigning.RequestID);
+                }
+            }
+
+            //update the status of all request in the list
+
+            foreach (var i in tmpRequestID)
+            {
+                Request request = db.Requests.Where(r => r.RequestID == i).FirstOrDefault();
+                listRequest.Add(request);
+                foreach (var rq in listRequest)
+                {
+                    rq.DeliveryStatusID = 2;
+                    db.Entry(rq).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+            }
+        }
+        #endregion
+
+        // Load trips arrived destination
+        [WebMethod]
+        public ActionResult ArrivedTrip()
+        {
+            List<Trip> trips = new List<Trip>();
+            int stationID = int.Parse(Session["StationID"].ToString());
+            trips = (from t in db.Trips
+                     where t.TripID ==
+                         ((from a in db.Assignings
+                           where a.RequestID ==
+                               ((from r in db.Requests
+                                 where r.DeliveryStatusID == 5 && r.ToLocation == stationID
+                                 select new { r.RequestID }).FirstOrDefault().RequestID)
+                           select new { a.TripID }).FirstOrDefault().TripID)
+                     select t).ToList();
+            return View(trips);
+        }
+
+        // Load trip depart from start station
+        [WebMethod]
+        public ActionResult DepartedTrip()
+        {
+            List<Trip> trips = new List<Trip>();
+            int stationID = int.Parse(Session["StationID"].ToString());
+            trips = (from t in db.Trips
+                     where t.TripID ==
+                         ((from a in db.Assignings
+                           where a.RequestID ==
+                               ((from r in db.Requests
+                                 where r.DeliveryStatusID == 4 && r.FromLocation == stationID
+                                 select new { r.RequestID }).FirstOrDefault().RequestID)
+                           select new { a.TripID }).FirstOrDefault().TripID)
+                     select t).ToList();
+            List<Trip> list = new List<Trip>();
+            foreach (var item in trips)
+            {
+                TimeSpan latetime = new TimeSpan();
+                TimeSpan estimateArrival = new TimeSpan();
+                latetime = (TimeSpan)(item.RealDepartureTime - item.EstimateDepartureTime);
+                estimateArrival = (TimeSpan)(item.EstimateArrivalTime + latetime);
+                item.EstimateArrivalTime = estimateArrival;
+                list.Add(item);
+            }
+            return View(list);
+        }
+
+        // Update assigned requests of departed trip after click button 'Xe đã chạy'
+        [HttpPost]
+        [WebMethod]
+        public void DepartedRequest(int tripID)
+        {
+            List<Assigning> _assignings = db.Assignings.ToList();
+            List<Request> listRequest = new List<Request>();
+            // list of requestIDs which departed
+            var tmpRequestIDs = new List<int>();
+            foreach (var assigning in _assignings)
+            {
+                if (assigning.TripID == tripID)
+                {
+                    // get list of request id that related to departed trip
+                    tmpRequestIDs.Add(assigning.RequestID);
+                }
+            }
+            // update real departed time and status of trip
+            var trip = db.Trips.Where(t => t.TripID == tripID).FirstOrDefault();
+            trip.RealDepartureTime = DateTime.Now.TimeOfDay;
+            trip.Status = "Đang chạy";
+            db.Entry(trip).State = EntityState.Modified;
+            db.SaveChanges();
+            //update the status of all request in the list
+            foreach (var i in tmpRequestIDs)
+            {
+                Request request = db.Requests.Where(r => r.RequestID == i).FirstOrDefault();
+                listRequest.Add(request);
+                foreach (var rq in listRequest)
+                {
+                    rq.DeliveryStatusID = 4;
+                    db.Entry(rq).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+            }
+        }
+
+        // Update assigned requests of arrived trip after click button 'Xe đã đến trạm'
+        [HttpPost]
+        [WebMethod]
+        public void ArrivedRequest(int tripID)
+        {
+            List<Assigning> _assignings = db.Assignings.ToList();
+            List<Request> listRequest = new List<Request>();
+            // list of requestIDs which arrived
+            var tmpRequestIDs = new List<int>();
+            foreach (var assigning in _assignings)
+            {
+                if (assigning.TripID == tripID)
+                {
+                    // get list of request id that related to arrived trip
+                    tmpRequestIDs.Add(assigning.RequestID);
+                }
+            }
+            // update real arrival time of trip
+            var trip = db.Trips.Where(t => t.TripID == tripID).FirstOrDefault();
+            trip.RealArrivalTime = DateTime.Now.TimeOfDay;
+            trip.Status = "Đã đến đích";
+            db.Entry(trip).State = EntityState.Modified;
+            db.SaveChanges();
+            //update the status of all request in the list
+            foreach (var i in tmpRequestIDs)
+            {
+                Request request = db.Requests.Where(r => r.RequestID == i).FirstOrDefault();
+                listRequest.Add(request);
+                foreach (var rq in listRequest)
+                {
+                    rq.ArrivedDate = DateTime.Now.Date;
+                    rq.DeliveryStatusID = 5;
+                    db.Entry(rq).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+            }
+        }
+    }
+}
